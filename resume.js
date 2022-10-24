4b7e6955b4ff1cc8768c175df97d64c4ce20db55
SANJAY0115 <115686334+SANJAY0115@users.noreply.github.com>
2022-10-24 13:41:40 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,64 @@
+const resume= [
+    {
+    "name" : "Sanjay" ,
+    "age" : "25",
+    "place": "Tirupur",
+    "languagesKnown" : "Tamil,English",
+    "phoneNumber" : "0123456789"
+},
+
+{
+    "sslc" : "90 %",
+    "hsc" : "92 %",
+    "ug" : "95 %"
+},
+{
+    "programmingLanguage" : "C ,C++ , JAVA ",
+    "operatingsystem" : "windos 8,11"
+     
+ },
+{
+   "Paper1" : "Artificial intelligence",
+   "Paper2" : "Internet Of Technologies"
+
+},
+{
+    1 : "DBMS",
+    2 : "OPERATING SYSTEM",
+    3 : "ARTIFICIAL INTELLIGENCE"
+},
+{
+    "a" : "E- Commerce Web Application",
+    "b" : "Students toolkit Web Application"
+},
+{
+   "contest" : " Second winner in ZOHO APPATHON",
+   
+
+}
+]
+
+
+
+
+console.log("PERSONAL DETAILS : ");
+console.log(resume[0]);
+console.log("BASICS ACADEMIC CREDENTIALS:");
+console.log(resume[1]);
+console.log("AREA OF INTEREST :");
+console.log(resume[2]);
+console.log("PAPER PRESENTATION:");
+console.log(resume[3]);
+console.log("TECHNICAL SKILLS :");
+console.log(resume[4]);
+console.log("PROJECTS :");
+console.log(resume[5]);
+console.log("ACHIEVEMENTS :");
+console.log(resume[6]);
+
+
+
+
+
+
+
